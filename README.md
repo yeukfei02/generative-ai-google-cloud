23526f54b769f675f221eec4d2c2d25aa98a5140
Donald Wu <yeukfei02@gmail.com>
2024-03-06 23:30:44 +0800

    feat: create project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # generative-ai-google-cloud
+
 generative-ai-google-cloud
+
+documentation: <>
+
+findAddress api url: <>
+
+getCheapFood api url: <>
+
+## Requirement
+
+- install yarn
+- install node (v18)
+- install python (v3.9)
+- install serverless
+- install gcloud cli
+
+## Testing and run
+
+```zsh
+// get bearer token
+$ gcloud auth print-identity-token
+
+// deploy to serverless
+$ yarn run deploy
+
+// generate .serverless folder
+$ yarn run package
+
+// remove serverless services in google cloud
+$ yarn run remove
+
+// check serverless.yml
+$ yarn run doctor
+```
---
+++ main.py
@@ -0,0 +1,65 @@
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel
+import vertexai.preview.generative_models as generative_models
+
+
+def get_itinerary_handler(request):
+    print(f"request = {request}")
+
+    response = {
+        "message": "get_itinerary",
+        "result": {}
+    }
+
+    if request.args:
+        day = request.args.get("day")
+        country = request.args.get("country")
+
+        result = generate_itinerary_by_gemini(day, country)
+        if result:
+            response = {
+                "message": "get_itinerary",
+                "result": result
+            }
+
+    headers = {
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    return (response, 200, headers)
+
+
+def generate_itinerary_by_gemini(day=7, country="Hong Kong"):
+    result = ""
+
+    vertexai.init(project="strange-vortex-416210", location="asia-southeast1")
+
+    model = GenerativeModel("gemini-1.0-pro-001")
+
+    responses = model.generate_content(
+        f"""
+            Generate a {day}-day itinerary for a trip to {country}.
+            Show attractions, food places and restaurants with the price, location, opening hours, contact information, and website.
+        """,
+        generation_config={
+            "max_output_tokens": 2048,
+            "temperature": 0.3,
+            "top_p": 1
+        },
+        safety_settings={
+            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        stream=True,
+    )
+    print(f"responses = {responses}")
+
+    if responses:
+        for response in responses:
+            print(f"response.text = {response.text}")
+
+            result += response.text
+
+    return result
